be5fe214aa22dd8ea65f45b2bb4a1c8b1b9ccda4
Supreeti Kumari <78208708+SupreetiKumari@users.noreply.github.com>
2022-08-24 12:52:56 +0530

    Update code1.vhdl

--- code1.vhdl
+++ code1.vhdl
@@ -36,3 +36,40 @@ begin
     when "0111" => LED_out <= "0001111"; -- "7" 
     when "1000" => LED_out <= "0000000"; -- "8"     
     when "1001" => LED_out <= "0000100"; -- "9"
+
+process(clock_100Mhz,reset)
+begin 
+    if(reset='1') then
+        refresh_counter <= (others => '0');
+    elsif(rising_edge(clock_100Mhz)) then
+        refresh_counter <= refresh_counter + 1;
+    end if;
+end process;
+ LED_activating_counter <= refresh_counter(19 downto 18);
+    
+/* process(LED_activating_counter)
+begin
+    case LED_activating_counter is
+    when "00" =>
+        Anode_Activate <= "0111"; 
+        -- activate LED1 and Deactivate LED2, LED3, LED4
+        LED_BCD <= displayed_number(15 downto 12);
+        -- the first hex digit of the 16-bit number
+    when "01" =>
+        Anode_Activate <= "1011"; 
+        -- activate LED2 and Deactivate LED1, LED3, LED4
+        LED_BCD <= displayed_number(11 downto 8);
+        -- the second hex digit of the 16-bit number
+    when "10" =>
+        Anode_Activate <= "1101"; 
+        -- activate LED3 and Deactivate LED2, LED1, LED4
+        LED_BCD <= displayed_number(7 downto 4);
+        -- the third hex digit of the 16-bit number
+    when "11" =>
+        Anode_Activate <= "1110"; 
+        -- activate LED4 and Deactivate LED2, LED3, LED1
+        LED_BCD <= displayed_number(3 downto 0);
+        -- the fourth hex digit of the 16-bit number    
+    end case;
+end process; */
+    
